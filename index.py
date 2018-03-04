13cead016b87589dc945eea53443fc10a507b75f
James <james@linnegar.com>
2018-03-04 09:48:08 +0000

    Change the colour of the ghost

--- index.py
+++ index.py
@@ -42,7 +42,8 @@ feet2 = [
 ]
 
 class Ghost:
-    def __init__(self, canvas, x=1, y=1):
+    def __init__(self, canvas, x=1, y=1, color):
+            self.color = color
             self.position = (x, y)
             self.canvas = canvas
             self.flip = False
@@ -56,21 +57,22 @@ class Ghost:
             self.canvas.set_pixel(rowIndex+y, colIndex+x, r,g,b)
 
     def draw(self):
-        self.updateCanvas(body,1,1,255,0,0)
+        r, g, b = self.color
+        self.updateCanvas(body,1,1,r,g,b)
         self.updateCanvas(eyes,3,4,255,255,255)
         self.updateCanvas(pupils,4,6,0,0,138)
 
         if(self.flip):
-          self.updateCanvas(feet2, 0,13,255,0,0)
+          self.updateCanvas(feet2, 0,13,r,g,b)
         else:
-          self.updateCanvas(feet,0,13,255,0,0)
+          self.updateCanvas(feet,0,13,r,g,b)
 
         self.flip = not self.flip
 
     def update(self):
         return
 
-ghost = Ghost(unicornhathd,1,1)
+ghost = Ghost(unicornhathd,1,1, (0,255,0))
 
 while True:
   unicornhathd.clear()
