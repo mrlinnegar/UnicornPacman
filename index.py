0317f7273a28c29248548827120273366f1fbbda
James Linnegar <jlinnega@thoughtworks.com>
2018-08-22 21:22:48 +0100

    no threading

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 import unicornhathd as display
 import time
 from ghost.Ghost import Ghost
-from threading import Thread
 from time import sleep
 
 
@@ -12,36 +11,19 @@ pinky = (244,197,212)
 blinky = (235, 0, 0)
 
 
-ghost = Ghost(display, blinky ,1,1)
-
-
-class DrawThread(Thread):
-    counter = 0
-    def __init__(self):
-        Thread.__init__(self)
-        self.daemon = True
-        self.start()
-
-    def run(self):
-        while True:
-            display.clear()
-            ghost.update()
-            ghost.draw()
-            display.show()
-
-        display.off()
-
+ghost = Ghost(display, inky ,1,1)
 
 
 try:
     print("Starting Ghost animation")
-    DrawThread()
+
     while True:
-        pass
-    display.off()
+        display.clear()
+        ghost.update()
+        ghost.draw()
+        display.show()
 
 except KeyboardInterrupt:
     sleep(1)
     display.off()
-    sleep(1)
     print("Ending ghost animation")
