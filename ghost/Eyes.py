5d884d7c749c9933576cc831714ac73bcb37591a
James Linnegar <jlinnega@thoughtworks.com>
2018-08-22 20:57:36 +0100

    Straight ahead

--- ghost/Eyes.py
+++ ghost/Eyes.py
@@ -1,7 +1,6 @@
 from .Drawable import Drawable
 import time
 class Eyes(Drawable):
-    closed = False
     lastUpdate = time.time()
     eyes = [
     [0,1,1,0,0,0,0,1,1,0],
@@ -24,4 +23,4 @@ class Eyes(Drawable):
 
     def draw(self):
         self.updateCanvas(self.eyes,3,4,255,255,255)
-        self.updateCanvas(self.pupils,3,6,0,0,138)
+        self.updateCanvas(self.pupils,4,6,0,0,138)
